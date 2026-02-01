10f9e394dd69297aa635011be7944ab14e1ef9ce
GRATITUD3 <gratitud3@mac.lan>
2026-02-01 07:58:32 -0800

    Add Hardhat/Foundry setup and OpenZeppelin dep

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  paths: {
+    sources: "./contracts",
+    tests: "./contracts/test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
+
+export default config;
